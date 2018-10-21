d5d0821101cb9b3de5dd807588e0bcd93a9fcc28
lysistrata1989 <ehamiltonhudson@gmail.com>
2018-10-20 21:16:37 -0400

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    singe_string = array.join()
+  elsif array.size == 2
+    double_string = array.join(" and ")
+  elsif array.size == 3
+    triple_string = array[0..-2].join(", ") + ", and " + array[-1]
+  else array.size > 4
+    last = array.pop
+    more_than_four = array.join(", ") << ", and " + last
+ end
 end
